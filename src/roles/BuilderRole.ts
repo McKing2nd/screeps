2435177df6538adc6b7618bb4879b9efd18aa9f4
Vince Mecking <v.mecking@gmail.com>
2018-02-12 09:44:45 +0100

    start of builder role

--- src/roles/BuilderRole.ts
+++ src/roles/BuilderRole.ts
@@ -1,17 +1,17 @@
+import { MyCreep } from './CreepRole'
 var roleHarvester = require('role.harvester');
-module.exports = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
+export class Builder extends MyCreep {
+    
+    run(creep: MyCreep): void {
 
         if(creep.memory.role =='builder' && creep.memory.target != undefined && creep.room.name != creep.memory.target) {
-            var exit = creep.room.findExitTo(creep.memory.target);
+            var exit: Room = creep.room.findExitTo<Room>(creep.memory.target);
             creep.moveTo(creep.pos.findClosestByPath(exit));
         } else {
             if(creep.isWorking()) {
-                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                var targets: ConstructionSite[] = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if(targets.length) {
-                    target = creep.pos.findClosestByPath(targets);
+                    var target: ConstructionSite = creep.pos.findClosestByPath(targets);
                     if(creep.build(target) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
